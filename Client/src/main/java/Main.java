e5da356b385ac414ff22c8a89e7b5f2eef6c168e
AnnT <annatokaeva@mail.ru>
2022-06-22 23:31:47 +0300

    Init commit

---
+++ Client/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8080;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in =
+                     new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                out.println("TestSignal" );
+                String resp = in.readLine();
+                System.out.println(resp);
+
+
+
+        } catch (ConnectException e) {
+            System.out.println("No connect");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
